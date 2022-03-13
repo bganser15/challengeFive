b4d31a96f635e62a40de5dae9d218336a40a103b
Brittany Ganser <15brganser@gmail.com>
2022-03-13 15:01:14 -0600

    html tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,29 @@
-//DateTime.now().toLocaleString()	//=>	"3/12/2022"
-
-//DateTime.now().toFormat('MMMM dd, yyyy')	//=>	"March 12, 2022"
-
 var DateTime = luxon.DateTime;
 var currentDate = DateTime.now().toFormat("MMM dd, yyyy");
 var currentHour = DateTime.now().hour;
-//displays current dateat top of scheduler
+//displays current date at top of scheduler
 $("#currentDay").text(currentDate);
-
+schedule = {
+  textHour: [],
+  plans: [],
+};
+//function to set timeblocks to color based on current hour
 var setBlockColors = function () {
   $(".timeBlock").each(function () {
     var hourBlock = $(this).attr("id");
-    console.log($(this).attr("id"));
-    console.log(currentHour);
     $(this).removeClass("past");
     $(this).removeClass("present");
     $(this).removeClass("future");
     if (hourBlock == currentHour) {
       $(this).addClass("present");
-      console.log("now");
     } else if (hourBlock < currentHour) {
-      //$(this).removeClass("present");
-      //$(this).removeClass("future");
       $(this).addClass("past");
     } else if (hourBlock > currentHour) {
-      //$(this).removeClass("present");
-      //$(this).removeClass("past");
       $(this).addClass("future");
     }
   });
 };
 
 setBlockColors();
+//storeTask();
+saveTask();
