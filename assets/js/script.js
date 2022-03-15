abd02150dab4936b2459fa21876640a784637449
Brittany Ganser <15brganser@gmail.com>
2022-03-14 23:18:33 -0600

    save input to local storage with button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var currentDate = DateTime.now().toFormat("MMM dd, yyyy");
 var currentHour = DateTime.now().hour;
 //displays current date at top of scheduler
 $("#currentDay").text(currentDate);
-schedule = {
-  textHour: [],
-  plans: [],
-};
+
+var inputText;
+var hour;
+
 //function to set timeblocks to color based on current hour
 var setBlockColors = function () {
   $(".timeBlock").each(function () {
@@ -23,7 +23,38 @@ var setBlockColors = function () {
     }
   });
 };
+var inputTasks = function () {
+  //gets text value after user clicks off textarea
+  $(".dailyPlans").on("blur", function () {
+    inputText = $(this).val();
+    console.log(inputText);
+  });
+};
 
+var saveTasks = function () {
+  $(".btn").on("click", function (event) {
+    hour = $(this).parent().parent().attr("id");
+    localStorage.setItem(hour, inputText);
+  });
+};
+
+var loadTasks = function () {
+  //var updateHour = localStorage.getItem("hour");
+  //var updateText = localStorage.getItem(JSON.stringify(inputText));
+  //var timeBlockEl = $(".timeBlock").attr("id");
+  $(".timeBlock").each(function () {
+    var eachId = $(this).attr("id");
+    //console.log(eachId);
+    var updateHour = localStorage.getItem(eachId);
+    console.log(updateHour);
+  });
+};
+
+//create blur function to input into textarea done
+//on save button click set time block hour and plans into an object
+//on save button click push that current object into locaql storage
+//create load tasks function to stringify data and display on page
 setBlockColors();
-//storeTask();
-saveTask();
+inputTasks();
+saveTasks();
+loadTasks();
